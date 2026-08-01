76a1b9c2938dab7a93e3daee635783a79542f8bf
Milin Patel <milinp551@gmail.com>
2026-08-01 12:48:08 +0530

    Time: 6 ms (22.67%) | Memory: 19.6 MB (51.20%) - LeetSync

---
+++ 684-redundant-connection/redundant-connection.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        t = "".join(map(chr,range(1001)))
+        for u,v in edges:
+            if t[u] == t[v]:
+                return [u,v]
+            t = t.replace(t[u],t[v])
